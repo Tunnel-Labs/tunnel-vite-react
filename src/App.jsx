3ff3f047d60fa9c6c97dc85d1d8b43710ce1b20a
David Thomas <github@davthom.xyz>
2024-01-29 17:40:29 -0500

    feat: add Tunnel

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import { TunnelToolbar } from '@tunnel/react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      {(
+          process.env.NODE_ENV !== "development" 
+          // Uncomment the following line to disable the Tunnel Toolbar in production
+          // && process.env.RAILWAY_GIT_BRANCH !== 'main'
+          ) && (
+          <TunnelToolbar 
+            projectId={process.env.TUNNEL_PROJECT_ID}
+            branch={process.env.RAILWAY_GIT_BRANCH}
+          />
+        )}
+    </>
+  )
+}
+
+export default App
